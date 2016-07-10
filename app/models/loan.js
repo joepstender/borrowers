2eac01901e05ebde3eb7271f018fac6590c5bc84
Joep Stender <joep@stender.nl>
2016-07-10 21:43:23 +0200

    changes up to page 60

---
+++ app/models/loan.js
@@ -0,0 +1,12 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { belongsTo } from 'ember-data/relationships';
+// import { hasMany } from 'ember-data/relationships';
+
+export default Model.extend({
+  notes: attr('string'),
+  returned: attr('boolean'),
+  createdAt: attr('date'),
+  friend: belongsTo('friend'),
+  article: belongsTo('article')
+});
